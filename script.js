22e26a45d0ee9a250a1a91395096b34f41966f00
annemarywynter1986 <missannemary1@gmail.com>
2023-05-23 15:56:21 -0400

    function new, retwisted some html and added style css

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const apiKey = "5778367e5dfb4dcf8b6b1511a110cbe8";
 const url = `https://api.spoonacular.com/recipes/complexSearch`;
-const searchInpitEl=document.getElementById('#searchInput');
-const searchBtn = document.getElementById("#submit");
+// const searchForm=document.getElementById('form');
+// const searchButton=document.getElementById("#submit");
+const searchResultDiv= document.getElementById(".search-result");
+const container=document.querySelector(".container");
+let searchQuery='';
+
+function handleSubmit(event) {
+    event.preventDefault(); // Prevents the form from being submitted and the page from refreshing
+    
+    const searchQuery = event.target.querySelector('input').value;
+    console.log(searchQuery);
+  }
+  
+  const searchForm = document.getElementById('#search-form'); //
+  searchForm.addEventListener('submit', handleSubmit);
+//   if (searchForm !== null) {
+//     searchForm.addEventListener('click', myFunction);
+//   }
+  
+   handleSubmit();
+  ;
+
 
 function getApi() {
     fetch(url, {headers: {"x-api-key":apiKey }})
