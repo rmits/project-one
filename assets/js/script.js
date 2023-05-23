dc0f74f25f96064a30c5d07d2d5ebd24b9ec1677
annemarywynter1986 <missannemary1@gmail.com>
2023-05-23 19:31:03 -0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,50 @@
-const apiKey = "5778367e5dfb4dcf8b6b1511a110cbe8";
-const url = `https://api.spoonacular.com/recipes/complexSearch`;
-const searchResultDiv= document.getElementById(".search-result");
-const container=document.querySelector(".container");
-let searchQuery='';
-// const searchForm = document.getElementById('search-form');
+const API_KEY = "5778367e5dfb4dcf8b6b1511a110cbe8";
+const recipeListEl = document.getElementById("recipe-list");
 
+function displayRecipes(recipes) {
+  recipeListEl.innerHTML = "";
+  recipes.forEach((recipe) => {
+    const recipeItemEl = document.createElement("li");
+    recipeItemEl.classList.add("recipe-item");
+    recipeImageEl = document.createElement("img");
+    recipeImageEl.src = recipe.image;
+    recipeImageEl.alt = "recipe image";
 
-function getApi() {
-    fetch(url, {headers: {"x-api-key":apiKey }})
-    .then(function (response) {
-        console.log(response.status);
-        //  Conditional for the the response.status.
-        if (response.status !== 200) {
-          // Place the response.status on the page.
-          responseText.textContent = response.status;
-        }
-        return response.json();
-    })
-    .then(function (data) {
-        // Make sure to look at the response in the console and read how 404 response is structured.
-        console.log(data);
-      })};
-getApi();
+    recipeTitleEl = document.createElement("h2");
+    recipeTitleEl.innerText = recipe.title;
 
-// Get the input field
-var input = document.getElementById("inputText");
+    recipeIngredientsEl = document.createElement("p");
+    recipeIngredientsEl.innerHTML = `
+        <strong>Ingredients:</strong> ${recipe.extendedIngredients
+          .map((ingredient) => ingredient.original)
+          .join(", ")}
+    `;
 
-// Execute a function when the user presses a key on the keyboard
-input.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    console.log(event.target.value)
-  }
-});
+    recipeLinkEl = document.createElement("a");
+    recipeLinkEl.href = recipe.sourceUrl;
+    recipeLinkEl.innerText = "View Recipe";
+
+    recipeItemEl.appendChild(recipeImageEl);
+    recipeItemEl.appendChild(recipeTitleEl);
+    recipeItemEl.appendChild(recipeIngredientsEl);
+    recipeItemEl.appendChild(recipeLinkEl);
+    recipeListEl.appendChild(recipeItemEl);
+  });
+}
+
+async function getRecipes() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`
+  );
+
+  const data = await response.json();
+
+  return data.recipes;
+}
+
+async function init() {
+  const recipes = await getRecipes();
+  displayRecipes(recipes);
+}
+
+init();
